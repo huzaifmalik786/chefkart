6029d2979d6779cc2c6dfecd59d6a2901e83e723
Yogesh Chandra <002chandra.yogesh@gmail.com>
2022-12-26 18:04:07 +0530

    FAQ

--- pages/join-as-chef/index.tsx
+++ pages/join-as-chef/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import FAQ from "../../components/FAQ";
 import Layout from "../../components/layouts/Layout";
 
 type Props = {};
 
 const index = (props: Props) => {
-  return <Layout>index</Layout>;
+  return (
+    <Layout>
+      <FAQ />
+    </Layout>
+  );
 };
 
 export default index;
