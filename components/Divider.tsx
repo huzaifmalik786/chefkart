f027e54da144ba50df9bca890d1d995031ccdd9c
navdeep1840 <navikitu001@gmail.com>
2023-01-01 19:31:31 +0530

    carrer page updated

---
+++ components/Divider.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Styles from "../styles/components/divider.module.scss";
+
+type Props = {};
+
+const Divider = (props: Props) => {
+  return <div className={Styles.divider} />;
+};
+
+export default Divider;
