3d8a196c032822aa1f47f1189ad0092188916849
unknown <huzaif@heliverse.com>
2023-03-16 20:41:23 +0530

    video-issue-fixed

--- components/homepage/HasleFreeHiring.tsx
+++ components/homepage/HasleFreeHiring.tsx
@@ -27,7 +27,7 @@ const HasleFreeHiring = (props: Props) => {
   return (
     <div className={Styles.container}>
       <div className={Styles.phone_img}>
-        <video className={Styles.video} src="/phonevideo.mp4" loop autoPlay muted/>
+        <video className={Styles.video} src="https://chefkart-strapi-media.s3.ap-south-1.amazonaws.com/phonevideo_f8b73e4bc3.MP4?updated_at=2023-03-16T15:06:37.737Z" loop autoPlay muted/>
       {/* <Image src={active && width>450? "/mobile1.png": "/mobile2.png"} alt={props.data?.image?.data?.attributes?.alternativeText || "phone"} fill /> */}
         {/* <Image src={props.data?.image?.data?.attributes?.url || "/phone.png"} alt={props.data?.image?.data?.attributes?.alternativeText || "phone"} fill /> */}
       </div>
